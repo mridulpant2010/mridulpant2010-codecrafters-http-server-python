7d077e260be5274586284561f1be5e3cdee640a1
Mridul Pant <mridulpant2010@gmail.com>
2024-05-26 16:01:16 +0530

    refactor the code

--- app/main.py
+++ app/main.py
@@ -2,27 +2,31 @@
 import socket
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
-    
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind(("localhost", 4221))
-        server_socket.listen()
-        conn, addr = server_socket.accept()
+def create_server(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
+        socket.bind((host, port))
+        socket.listen()
+        conn, addr = socket.accept()
         with conn:
-            print('Connected by', addr)
+            print(f"Connected by {addr}")
             while True:
                 data = conn.recv(1024)
                 print(data)
                 if not data:
                     break
                 conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        
+
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+    HOST = "localhost"
+    PORT = "4221"
+    # Uncomment this to pass the first stage
+    #
+    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    # server_socket.accept() # wait for client
+    create_server(HOST,PORT)
 
 if __name__ == "__main__":
     main()
