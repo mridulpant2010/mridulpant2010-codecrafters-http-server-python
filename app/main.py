49181a337a8d72b650861c78efd763081ba77234
Mridul Pant <mridulpant2010@gmail.com>
2024-05-26 16:03:50 +0530

    changed the port from str to int

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     HOST = "localhost"
-    PORT = "4221"
+    PORT = 4221
     # Uncomment this to pass the first stage
     #
     # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
